a5ab4f0303ddbc0cf4d9e3d88581dc4af7602b66
Atelier144 <amestric16@gmail.com>
2020-03-27 15:01:02 +0900

    update

--- Assets/Scripts/TypingManager.cs
+++ Assets/Scripts/TypingManager.cs
@@ -13,22 +13,26 @@ public class Sentence
 
 public class TypingManager : MonoBehaviour
 {
-    [SerializeField] TextMeshPro textMeshProTitle;
-    [SerializeField] TextMeshPro textMeshProQuestion;
+    [SerializeField] TextMeshProUGUI textMeshProTitle;
+    [SerializeField] TextMeshProUGUI textMeshProQuestion;
+
+    [SerializeField] Sentence[] sentences;
 
     int typingCount;
     int typingTime;
     int typingSpeed;
 
     string currentTitle;
-    string currentQuestion = "tonarinokyakuhayokukakikuukyakuda";
+    string currentQuestion = "choukousokutaipingu";
 
-    char[] currentQuestionArray;
+    List<char> listCurrentQuestion = new List<char>();
+
+    int currentQuestionIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentQuestionArray = currentQuestion.ToCharArray();
+        SetSentence();
         StartCoroutine(TestAutoTyping());
     }
 
@@ -37,15 +41,28 @@ public class TypingManager : MonoBehaviour
     {
         if (true)
         {
-            string[] keys = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-            foreach(string key in keys)
+
+            switch(Type())
             {
-                if (Input.GetKeyDown(key))
-                {
+                case 1:
                     typingCount++;
+                    if (listCurrentQuestion[currentQuestionIndex] == '}')
+                    {
+                        currentQuestionIndex = 1;
+                        typingSpeed = 3000 * typingCount / typingTime;
+                        Debug.Log("タイピング速度：" + typingSpeed + "kpm");
+                        typingTime = 0;
+                        typingCount = 0;
+                        SetSentence();
+                    }
+                    
                     UpdateTextMeshProQuestion();
-                }
+                    break;
+                case 2:
+                    Debug.LogError("ミスタイプ");
+                    break;
             }
+
         }
     }
 
@@ -55,16 +72,314 @@ public class TypingManager : MonoBehaviour
     }
     void UpdateTextMeshProQuestion()
     {
-        typingSpeed = typingCount * 3000 / typingTime;
-        Debug.Log("タイピング速度：" + typingSpeed + "kpm");
+        string question = "<style=Typed>";
+        int i=1;
+        while(listCurrentQuestion[i]!='}')
+        {
+            
+            if(currentQuestionIndex == i){
+                question += "</style><style=Untyped>";
+            }
+            question += listCurrentQuestion[i];
+            i++;
+        }
+        question += "</style>";
+        textMeshProQuestion.text = question;
+       
+
+        
     }
 
+    void SetSentence()
+    {
+        Sentence sentence = sentences[Random.Range(0, sentences.Length)];
+        currentTitle = sentence.title;
+        currentQuestion = sentence.question;
+
+        listCurrentQuestion.Clear();
+        listCurrentQuestion.Add('{');
+        char[] characters = currentQuestion.ToCharArray();
+        foreach(char character in characters)
+        {
+            listCurrentQuestion.Add(character);
+        }
+        listCurrentQuestion.Add('}');
+        currentQuestionIndex = 1;
+        UpdateTextMeshProQuestion();
+        textMeshProTitle.text = currentTitle;
+    }
     IEnumerator TestAutoTyping()
     {
         while (true)
         {
             yield return new WaitForSeconds(0.5f);
-            UpdateTextMeshProQuestion();
         }
+    }
+
+    int Type()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'a')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'b')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'c')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'd')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'e')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'f')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'g')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'h')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'i')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'j')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'k')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'l')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'm')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'n')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'o')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'p')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'q')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'r')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 's')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 't')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'u')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'v')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'w')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'x')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'y')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            if(listCurrentQuestion[currentQuestionIndex]== 'i') //「い」のタイピングか？
+            {
+                char prevChar = listCurrentQuestion[currentQuestionIndex - 1];
+                if(prevChar == '{' || prevChar == 'a' || prevChar == 'i' || prevChar == 'u' || prevChar == 'e' || prevChar == 'o')
+                {
+                    listCurrentQuestion.Insert(currentQuestionIndex, 'y');
+                    currentQuestionIndex++;
+                    return 1;
+                }
+                if (prevChar == 'n') //前が「ん」の可能性があるか？
+                {
+                    if (listCurrentQuestion[currentQuestionIndex - 2] == 'n') //その前が「ん」か？
+                    {
+                        if (listCurrentQuestion[currentQuestionIndex - 3] == 'n') //「ん」の後が本当に「い」か？
+                        {
+                            return 2;
+
+                        }
+                        else
+                        {
+                            listCurrentQuestion.Insert(currentQuestionIndex, 'y');
+                            currentQuestionIndex++;
+                            return 1;
+                        }
+                    }
+                }
+            }
+            return 2;
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (listCurrentQuestion[currentQuestionIndex] == 'z')
+            {
+                currentQuestionIndex++;
+                return 1;
+            }
+            return 2;
+        }
+        return 0;
+
     }
 }
